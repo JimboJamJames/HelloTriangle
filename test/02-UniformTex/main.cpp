50ef9c9bcf87619c194e2ff263968c1537d3f4df
s179134 <s179134@SEA-7FF209>
2017-08-18 12:37:03 -0700

    did some more stuff

--- test/02-UniformTex/main.cpp
+++ test/02-UniformTex/main.cpp
@@ -12,8 +12,7 @@ int main()
 	{ { -.5, -.5,  0, 1 },{ 1,0,1,1 } },   //2
 	{ { .5, -.5,  0, 1 },{ 1,0,0,1 } } }; //3
 
-	unsigned idxs[6] = { 1,2,3,
-		0,3,1 };
+	unsigned idxs[6] = { 1,2,3,0,3,1 };
 
 	Geometry g = makeGeometry(verts, 4, idxs, 6);
 	
@@ -21,28 +20,35 @@ int main()
 
 	const char* vsource = // once per vertex
 		"#version 450\n"
-		"layout(location = 0) in vec4 position;\n"
+		"layout(location = 0) in vec4 position; \n"
 		"layout(location = 1) in vec4 color;\n"
 		"layout(location = 0) uniform float time;\n"
-		"flat out vec4 vColor;"
+		"out vec4 vColor;"
 		"void main ()\n"
 		"{\n"
 		"gl_Position = position;\n"
+		"gl_Position.x -= sin(time*3+position.y)/1.0;\n"
+		"gl_Position.y *= sin(time*3+position.y)/1.0;\n"
 		"vColor = color;"
 		"}\n";
 
 	const char* fsource = // once per pixel
 		"#version 450\n"
 		"out vec4 outColor;\n"
-		"flat in vec4 vColor;"
+		"layout(location = 0) uniform float time;\n"
+		"layout(location = 1) uniform int tog;\n"
+		"in vec4 vColor;"
 		"void main ()\n"
 		"{\n"
-		"outColor = vColor;\n"
+		"if(tog == 1)\n"
+		"outColor = 12.0 - vColor;\n"
+		"else outColor = vColor;\n"
+		"outColor.r = 0.6+sin(time*1200+gl_FragCoord.x/6)/1.0;\n"
 		"}\n";
 
 	Shader s = makeShader(vsource, fsource);
 
-	Framebuffer f = { 0, 800, 600 };
+	Framebuffer f = { 0, 900, 700 };
 
 	while (context.step())
 	{
