0112c01db2b38f40bff7a47e23cc811b460f4c98
s179134 <s179134@SEA-8D0A7B>
2017-08-24 09:34:25 -0700

    somewhat caught up

--- include/graphics/draw.h
+++ include/graphics/draw.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "glm\glm.hpp"
 
 struct Geometry;
 struct Shader;
@@ -6,12 +7,39 @@ struct Framebuffer;
 struct Texture;
 
 void s0_draw(const Framebuffer &f,
-			 const Shader &s,
-			 const Geometry &g);
+	const Shader &s,
+	const Geometry &g);
 
-void clearFramebuffer(const Framebuffer &fb);
 
-void setUniform(const Shader &shader, int location, float value);
-void setUniform(const Shader &shader, int location, int value);
+void clearFramebuffer(const Framebuffer &f);
 
-void setUniform(const Shader &shader, int location, const Texture &value, int slot);
+void setUniform(const Shader &s, int location, float value);
+void setUniform(const Shader &s, int location, int value);
+
+void setUniform(const Shader &s, int location, const Texture &value, unsigned slot);
+
+
+namespace __internal
+{
+	void t_setUniform(const Shader &s, int &loc_io, int &tex_io, float val);
+	void t_setUniform(const Shader &s, int &loc_io, int &tex_io, int val);
+	void t_setUniform(const Shader &s, int &loc_io, int &tex_io, const Texture &val);
+
+	void t_setUniform(const Shader &s, int &loc_io, int &tex_io, const glm::vec3 &val);
+	void t_setUniform(const Shader &s, int &loc_io, int &tex_io, const glm::vec4 &val);
+	void t_setUniform(const Shader &s, int &loc_io, int &tex_io, const glm::mat4 &val);
+}
+
+template<typename T, typename ...U>
+void setUniforms(const Shader &s, int &loc_io, int &tex_io, const T &val, U &&... uniforms)
+{
+	__internal::t_setUniform(s, loc_io, tex_io, val);
+
+	setUniforms(s, loc_io, tex_io, uniforms...);
+}
+
+template<typename T>
+void setUniforms(const Shader &s, int &loc_io, int &tex_io, const T &val)
+{
+	__internal::t_setUniform(s, loc_io, tex_io, val);
+}
--- source/draw.cpp
+++ source/draw.cpp
@@ -1,10 +1,12 @@
 #include "graphics\draw.h"
 #include "graphics\RenderObjects.h"
 #include "glinc.h"
+#include "glm\glm.hpp"
+#include "glm/gtc/type_ptr.hpp"
 
 void s0_draw(const Framebuffer &f,
-			 const Shader &s,
-			 const Geometry &g)
+	const Shader &s,
+	const Geometry &g)
 {
 	glBindFramebuffer(GL_FRAMEBUFFER, f.handle);
 	glUseProgram(s.handle);
@@ -23,19 +25,56 @@ void setUniform(const Shader &s, int location, float value)
 {
 	glProgramUniform1f(s.handle, location, value);
 }
+
 void setUniform(const Shader &s, int location, int value)
 {
-	glProgramUniform1f(s.handle, location, value);
+	glProgramUniform1i(s.handle, location, value);
 }
+
 void clearFramebuffer(const Framebuffer &f)
 {
 	glBindFramebuffer(GL_FRAMEBUFFER, f.handle);
 	glClear(GL_COLOR_BUFFER_BIT);
 	glBindFramebuffer(GL_FRAMEBUFFER, 0);
 }
-void setUniform(const Shader &shader, int location, const Texture &value, int slot)
+
+void setUniform(const Shader &s, int location, const Texture &value, unsigned slot)
 {
 	glActiveTexture(GL_TEXTURE0 + slot);
 	glBindTexture(GL_TEXTURE_2D, value.handle);
-	glProgramUniform1i(shader.handle, location, slot);
+
+	glProgramUniform1i(s.handle, location, slot);
+
+	//glActiveTexture(0);
+}
+
+namespace __internal
+{
+	void t_setUniform(const Shader &s, int &loc_io, int &tex_io, float val)
+	{
+		glProgramUniform1f(s.handle, loc_io++, val);
+	}
+	void t_setUniform(const Shader &s, int &loc_io, int &tex_io, int val)
+	{
+		glProgramUniform1i(s.handle, loc_io++, val);
+	}
+	void t_setUniform(const Shader &s, int &loc_io, int &tex_io, const Texture &val)
+	{
+		glActiveTexture(GL_TEXTURE0 + tex_io);
+		glBindTexture(GL_TEXTURE_2D, val.handle);
+		glProgramUniform1i(s.handle, loc_io++, tex_io++);
+	}
+
+	void t_setUniform(const Shader &s, int &loc_io, int &tex_io, const glm::vec3 &val)
+	{
+		glProgramUniform3fv(s.handle, loc_io++, 1, glm::value_ptr(val));
+	}
+	void t_setUniform(const Shader &s, int &loc_io, int &tex_io, const glm::vec4 &val)
+	{
+		glProgramUniform4fv(s.handle, loc_io++, 1, glm::value_ptr(val));
+	}
+	void t_setUniform(const Shader &s, int &loc_io, int &tex_io, const glm::mat4 &val)
+	{
+		glProgramUniformMatrix4fv(s.handle, loc_io++, 1, 0, glm::value_ptr(val));
+	}
 }
